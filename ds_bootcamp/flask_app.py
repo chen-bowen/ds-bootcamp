1b1ee4fb9e493561bd02a2320e4529ca7c494087
Bowen Chen <chenbowen184@gmail.com>
2019-05-31 13:49:26 -0700

    add simple model

--- ds_bootcamp/flask_app.py
+++ ds_bootcamp/flask_app.py
@@ -1,12 +1,29 @@
 from flask import Flask
+import yaml
+import os
+from flask import Flask, jsonify
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def create_app():
     app = Flask(__name__)
 
+    with open(os.path.join(dir_path, 'yamls/simple_model.yaml'), 'r') as f:
+       simple_model = yaml.load(f, Loader=yaml.SafeLoader)
+
     @app.route("/bowen_hello")
     def hello_world():
         return "Hello, World", 200
-    
+
+    @app.route("/simple_model/<submodel>/<x_1>")
+    def yaml_model(submodel, x_1):
+        try:
+            model = simple_model[submodel]
+        except KeyError:
+            return "Model Not Found", 404
+        dict_ = {'score': model['a1'] + model['b1']*int(x_1)}
+        return jsonify(dict_)
+
     @app.route("/bowen_bye")
     def goodbye_world():
         return "Goodbye, World", 200
--- tests/bowenc_test.py
+++ tests/bowenc_test.py
@@ -4,6 +4,15 @@ def test_get_metadata(client):
     res = client.get("/bowen_hello")
     assert res.status_code == 200
 
-def test_get_metadata(client):
+def test_get_metadata2(client):
     res = client.get("/bowen_bye")
     assert res.status_code == 200
+
+def test_get_simple_model(client):
+    res = client.get("/simple_model/submodel_1/10")
+    assert  res.status_code == 200
+    assert res.json['score'] == 10
+
+def test_no_model(client):
+    res = client.get("/simple_model/submodel_3/10")
+    assert res.status_code == 404
