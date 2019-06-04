584db439fa65eaa5cdcdebdeb50902ab365da2ae
Bowen Chen <chenbowen184@gmail.com>
2019-06-04 14:10:16 -0700

    update tests, add more functionality

--- ds_bootcamp/flask_app.py
+++ ds_bootcamp/flask_app.py
@@ -4,6 +4,8 @@ import os
 from flask import Flask, jsonify
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+MAX_VALUE = 10000
+MIN_VALUE = 0
 
 def create_app():
     app = Flask(__name__)
@@ -17,11 +19,18 @@ def create_app():
 
     @app.route("/simple_model/<submodel>/<x_1>")
     def yaml_model(submodel, x_1):
+        x_1 = int(x_1)
+        if x_1 > MAX_VALUE:
+            return f"Value {x_1} > {MAX_VALUE}", 400
+        
+        if x_1 < MIN_VALUE:
+            return f"Value {x_1} < {MIN_VALUE}", 400
+
         try:
             model = simple_model[submodel]
         except KeyError:
             return "Model Not Found", 404
-        dict_ = {'score': model['a1'] + model['b1']*int(x_1)}
+        dict_ = {'score': model['a1'] + model['b1'] * x_1}
         return jsonify(dict_)
 
     @app.route("/bowen_bye")
--- tests/bowenc_test.py
+++ tests/bowenc_test.py
@@ -1,4 +1,6 @@
 import pytest
+from hypothesis import strategies as st, given, reproduce_failure
+from ds_bootcamp.flask_app import MAX_VALUE, MIN_VALUE
 
 def test_get_metadata(client):
     res = client.get("/bowen_hello")
@@ -11,8 +13,24 @@ def test_get_metadata2(client):
 def test_get_simple_model(client):
     res = client.get("/simple_model/submodel_1/10")
     assert  res.status_code == 200
-    assert res.json['score'] == 10
+    assert res.json['score'] == 25
 
 def test_no_model(client):
     res = client.get("/simple_model/submodel_3/10")
     assert res.status_code == 404
+
+@given(x=st.integers(min_value=MIN_VALUE, max_value=MAX_VALUE))
+def test_simple_model_hypothesis(client, x):
+    res =  client.get(f"/simple_model/submodel_1/{x}")
+    assert res.status_code == 200
+    assert res.json['score'] == 5 + 2*x
+
+@given(x=st.integers(min_value=MAX_VALUE + 1))
+def test_simple_model_high_value(client, x):
+    res =  client.get(f"/simple_model/submodel_1/{x}")
+    assert res.status_code == 400
+
+@given(x=st.integers(max_value=MIN_VALUE - 1))
+def test_simple_model_low_value(client, x):
+    res =  client.get(f"/simple_model/submodel_1/{x}")
+    assert res.status_code == 400
